fcdb1120e0934bcb71fa3a02f8da2b11088411a3
Toumal Mojumder <32560686+toumalmojumder@users.noreply.github.com>
2021-11-06 03:56:38 +0600

    create a new user

--- employees/resources/views/layouts/main.blade.php
+++ employees/resources/views/layouts/main.blade.php
@@ -12,8 +12,8 @@
     <title>Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" 
-    integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+    integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link
@@ -69,7 +69,7 @@
                 </div>
             </li>
 
-       
+
 
             <!-- Divider -->
             <hr class="sidebar-divider">
@@ -83,7 +83,7 @@
                 </a>
                 <div id="collapseUser" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="buttons.html">User</a>
+                        <a class="collapse-item" href="{{ route('users.index') }}">User</a>
                         <a class="collapse-item" href="cards.html">Role</a>
                         <a class="collapse-item" href="cards.html">Permission</a>
                     </div>
@@ -102,9 +102,6 @@
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                         <i class="fa fa-bars"></i>
                     </button>
-
-                   
-
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
 
@@ -113,7 +110,7 @@
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->username}}</span>
-                               
+
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
@@ -139,10 +136,8 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
                     <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>   
-                    </div>
-                    <div class="row">
+
+                 
                         @yield('content')
                     </div>
                 </div>
@@ -186,7 +181,7 @@
         </div>
     </div>
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ mix('js/app.js')}}"></script>   
+    <script src="{{ mix('js/app.js')}}"></script>
     <!-- Custom scripts for all pages-->
     <script src="{{asset('js/sb-admin.min.js')}}"></script>
 </body>
---
+++ employees/resources/views/users/create.blade.php
@@ -0,0 +1,23 @@
+
+@extends('layouts.main')
+
+@section('content')
+
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">User</h1>
+</div>
+
+<div class="card">
+    <div class="card-header">
+    <h4>
+        Create New User
+    </h4>
+    </div>
+<div class="row">
+    <div class="card-body">
+        form
+    </div>
+</div>
+</div>
+
+@endsection
